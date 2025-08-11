8ab852a6f53742837660d0d14a790bab9f80befd
Joao ricardo <joaoricardo@pop-os.bbrouter>
2025-08-11 16:07:49 -0300

    Ajuste de Texto

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
   return (
-  <h1>Bem Vindo, Sucesso my friend</h1>,
-  <h2> Novidade vem ai </h2>
+    <div>
+      <h1>Bem Vindo, Sucesso my friend</h1>
+      <h2> Novidade vem ai </h2>
+    </div>
 );
 }
 
